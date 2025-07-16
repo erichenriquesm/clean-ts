7664c96c1ecf4f5a65457c15e24117b6c1ae3bfa
eric-motta <eric@greenn.com.br>
2025-07-16 01:06:33 -0300

    improvement> adjusting unit tests with better assertions

--- src/Countries/tests/GetCountries.test.ts
+++ src/Countries/tests/GetCountries.test.ts
@@ -1,18 +1,23 @@
 import { GetCountries } from "../Services/GetCountries.ts";
 
-describe('Valida retorno para se obter os países', () => {
-    it("Deve retornar um array com países", async () => {
-        // Arrange - Preparo
-        const service = new GetCountries();
+describe("Valida retorno para se obter os países", () => {
+  it("Deve retornar um array com países", async () => {
+    // Arrange - Preparo
+    const service = new GetCountries();
 
-        // Act - Execução
-        const result:object[] = await service.exec();
-        const firtCountry: object = result[0];
+    // Act - Execução
+    const result: object[] = await service.exec();
+    const firtCountry: object = result[0];
 
-        // Assert - Verificação
-        expect(typeof result).toBe('object');
-        expect(firtCountry).not.toBeNull();
-        expect(firtCountry.hasOwnProperty('name')).toBeTruthy();
-        expect(firtCountry.hasOwnProperty('region')).toBeTruthy();
-    });
+    // Assert - Verificação
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBeGreaterThan(0);
+    expect(firtCountry).not.toBeNull();
+    expect(firtCountry).toEqual(
+      expect.objectContaining({
+        name: expect.any(Object),
+        region: expect.any(String),
+      })
+    );
+  });
 });
--- src/Countries/tests/GetCountriesController.test.ts
+++ src/Countries/tests/GetCountriesController.test.ts
@@ -1,19 +1,23 @@
 import { GetCountriesController } from "../Controllers/GetCountriesController";
 import { GetCountries } from "../Services/GetCountries.ts";
 
-describe('Valida retorno para se obter os países', () => {
-    it("Deve retornar um array com países", async () => {
-        // Arrange - Preparo
-        const controller = new GetCountriesController(new GetCountries());
+describe("Valida retorno para se obter os países", () => {
+  it("Deve retornar um array com países", async () => {
+    // Arrange - Preparo
+    const controller = new GetCountriesController(new GetCountries());
 
-        // Act - Execução
-        const result:object[] = await controller.exec();
-        const firtCountry: object = result[0];
+    // Act - Execução
+    const result: object[] = await controller.exec();
+    const firtCountry: object = result[0];
 
-        // Assert - Verificação
-        expect(typeof result).toBe('object');
-        expect(firtCountry).not.toBeNull();
-        expect(firtCountry.hasOwnProperty('name')).toBeTruthy();
-        expect(firtCountry.hasOwnProperty('region')).toBeTruthy();
-    });
+    // Assert - Verificação
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBeGreaterThan(0);
+    expect(firtCountry).toEqual(
+      expect.objectContaining({
+        name: expect.any(Object),
+        region: expect.any(String),
+      })
+    );
+  });
 });
